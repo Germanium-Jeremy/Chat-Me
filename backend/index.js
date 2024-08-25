a32a1328196551d30a3edc7efe28c230b6e35591
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-08-25 18:03:05 +0200

    complications deploy backend

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,26 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const userRoutes = require('./routes/userRoutes')
+const chatRoutes = require('./routes/chatRoutes')
+const simpleRoutes = require('./routes/simpleRoutes')
+const messageRoutes = require('./routes/messageRoutes')
+
+app.use(userRoutes)
+app.use(chatRoutes)
+app.use(messageRoutes)
+app.use(simpleRoutes)
+
+mongoose.connect(process.env.DB_STRING).then(() => {
+     console.log("Mongo connected successfully")
+}).catch((error) => {
+     console.log("Error connecting to mongo: ", error.message)
+})
+
 const server = http.createServer(app)
 const io = new Server(server, {
      cors: {
-          origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+          origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://chat-me-eight.vercel.app"],
           methods: ["GET", "POST"]
      }
 })
@@ -30,20 +46,6 @@ io.on("connection", (socket) => {
 
 })
 
-const userRoutes = require('./routes/userRoutes')
-const chatRoutes = require('./routes/chatRoutes')
-const simpleRoutes = require('./routes/simpleRoutes')
-const messageRoutes = require('./routes/messageRoutes')
-
-app.use(userRoutes)
-app.use(chatRoutes)
-app.use(messageRoutes)
-app.use(simpleRoutes)
-
-mongoose.connect(process.env.DB_STRING).then(() => {
-     console.log("Mongo connected successfully")
-}).catch((error) => {
-     console.log("Error connecting to mongo: ", error.message)
-})
+module.exports = server
 
 server.listen(process.env.PORT, () => console.log(`Server running on: http://localhost:${process.env.PORT}`))
